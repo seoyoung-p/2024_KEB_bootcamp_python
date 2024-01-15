ecfb157090aa2811093f81547a68fe6f82e272a0
seoyoung <psy20116@gmail.com>
2024-01-15 13:04:08 +0900

    v0.6 python day02

--- day02.py
+++ day02.py
@@ -1,4 +1,4 @@
-base_number = input('Input base number : ')
+base_number = int(input('Input base number : '))
 exponent_number = int(input('Input exponent number : '))
 print(f'밑은 {base_number}, 지수는{exponent_number}, 결과 값은 {base_number**exponent_number}')
 
