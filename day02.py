67dda64336a6e174b7f48f7d18728eb150377cfb
seoyoung <psy20116@gmail.com>
2024-01-15 14:14:36 +0900

    v0.9 python day02

--- day02.py
+++ day02.py
@@ -1,11 +1,8 @@
-first_number = int(input("First number: "))
-second_number = int(input("Second number: "))
-
-print(f'몫은 {divmod(first_number, second_number)[0]} 나머지는 {divmod(first_number, second_number)[1]}입니다')
-
-# first_number = int(input("First number: "))
-# second_number = int(input("Second number: "))
-#
-# quotient = first_number // second_number
-# remainder = first_number % second_number
-# print(f'몫은 {quotient} 나머지는 {remainder}입니다')
+dec = 65
+octal = 0o101 #8진수로 쓰고싶으면 0o
+hexadecimal = 0x41 #16진수로 쓰고싶으면 0x
+binary = 0b01000001 #이진수로 쓰고싶으면 0b
+print(dec,octal,hexadecimal,binary) #print 시 10진수로 변환하여 출력됨 (출력결과 65 65 65 65)
+print(chr(dec), chr(octal), chr(hexadecimal), chr(binary)) #A
+print(bin(dec), bin(octal), bin(hexadecimal), bin(binary)) #0b1000001
+print(ord('B'), ord('Z'), ord('a'), ord('2')) #66, 90, 97, 50
