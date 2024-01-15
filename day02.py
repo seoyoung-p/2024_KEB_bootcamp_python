cc5005dfc57b9ec9fcb9d0edd1ededf02fb72581
seoyoung <psy20116@gmail.com>
2024-01-15 10:44:54 +0900

    v0.3 python day02

---
+++ day02.py
@@ -0,0 +1,19 @@
+univ = "Inha university"
+print(univ)
+print(univ[5])
+# univ[5] = 'U' #immutable #불변
+# print(univ)
+subjects = ['python', 'c++', 'linux', 'data structure', 'database']
+print(subjects)
+print(subjects[3])
+subjects[3] = 'data structure & algorithm'  #mutable 가변성
+print(subjects)
+
+
+# print(0.1)
+# print(1e-1)
+# print(0.01)
+# print(1e-2)
+# print(3.141592)
+# print(21000)
+# print(21_000)
--- hello.py
+++ hello.py
@@ -1,1 +1,1 @@
-print("Hello Python~")
+print("Hello Python~") #alt+shift=f10
