4ff53a4538eba4f27c6b5a32900d0e1afd11b176
seoyoung <psy20116@gmail.com>
2024-01-15 14:30:10 +0900

    v1.0 python day02

--- day02.py
+++ day02.py
@@ -1,8 +1,4 @@
-dec = 65
-octal = 0o101 #8진수로 쓰고싶으면 0o
-hexadecimal = 0x41 #16진수로 쓰고싶으면 0x
-binary = 0b01000001 #이진수로 쓰고싶으면 0b
-print(dec,octal,hexadecimal,binary) #print 시 10진수로 변환하여 출력됨 (출력결과 65 65 65 65)
-print(chr(dec), chr(octal), chr(hexadecimal), chr(binary)) #A
-print(bin(dec), bin(octal), bin(hexadecimal), bin(binary)) #0b1000001
-print(ord('B'), ord('Z'), ord('a'), ord('2')) #66, 90, 97, 50
+# (100℉ - 32) * 5/9 = 3.778℃
+fahrenheit = float(input('Input Fahrenheit : '))
+print(f'fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit - 32.0) * 5.0/9.0):.2f}C')
+#.2f로 소수점 2번쨰자리까지 출력 (f스트링 서식)
