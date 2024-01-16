65be355c63c3def8b4ee1c822f8d66973d7567f6
seoyoung <psy20116@gmail.com>
2024-01-16 13:37:25 +0900

    v2.1 assignment day03

--- dya03.py
+++ dya03.py
@@ -1,8 +1,9 @@
-#preview
+# preview
 subjects = "python c++ database linux"
+print('%e' % 0.7045)
+print(subjects.isalnum())
 subject = input("수강신청과목 입력 : ")
-
 try:
-    print(f'해당 과목이 존재합니다. 위치는 {subjects.index(subject)}번 인덱스 입니다.')
+    print(f"해당 과목이 존재합니다. 위치는 {subjects.index(subject)}번 인덱스입니다.")
 except ValueError:
-    print('해당 과목이 존재하지 않습니다')
+    print('해당과목이 존재하지 않습니다')
