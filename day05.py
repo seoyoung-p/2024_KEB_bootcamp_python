f1b8d26d2cc5710e20876176f867d8fb229dd22c
seoyoung <psy20116@gmail.com>
2024-01-18 14:55:36 +0900

    v5.0 python day05

--- day05.py
+++ day05.py
@@ -1,11 +1,10 @@
-def squares(n):
-    return n * n
+# def squares(n):
+#     return n * n
 
 even_numbers = [i for i in range(51) if i % 2 == 0]
 print(even_numbers)
-# print(tuple(map(squares, even_numbers)))
-# print(tuple(map(lambda x : x**2, even_numbers))) #함수가 들어가는 자리에 lamda 함수를 사용
+#print(tuple(map(squares, even_numbers)))
 
-z = lambda x : pow(x, 2)
+#print(tuple(map(lambda x: x**2, even_numbers)))
+z = lambda x: pow(x, 2)
 print(tuple(map(z, even_numbers)))
-
