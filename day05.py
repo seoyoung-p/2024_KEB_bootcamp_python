213d39cd529e8b71ba695e4b51f6bf82471a01ad
seoyoung <psy20116@gmail.com>
2024-01-18 14:18:45 +0900

    v4.7 python day05

--- day05.py
+++ day05.py
@@ -1,14 +1,8 @@
-def squares(*n) -> list:
-    """
-    넘겨 받은 수치 데이터들을 거듭제곱 값을 리스트에 담아서 리턴
-    :param n: tuple
-    :return: list
-    """
-    return [pow(i, 2) for i in n ]
-    #return n * n
+numbers = ["7", "-11", "3"]
+print(sum(map(int, numbers)))
 
-def run_function(f, *number) -> list:
-    return f(*number)
-
-print(squares(7, 5,2))
-print(run_function(squares, 9,10))
+# numbers = ["7", "-11", "3"]
+# hap = 0
+# for number in numbers:
+#     hap = hap + int(number)
+# print(hap)
