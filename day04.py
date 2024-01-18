551b5358307278a0439de0bcf0916112e341dde3
seoyoung <psy20116@gmail.com>
2024-01-18 09:28:52 +0900

    v4.3 assignment day05

--- day04.py
+++ day04.py
@@ -1,37 +1,26 @@
-import random
-drinks_foods = {"위스키": "초콜릿", "와인": "치즈", "소주": "삽겹살", "고량주": "양꼬치"}
-# print(drinks_foods)
-# print(drinks_foods.pop("고량주"))
-# print(drinks_foods)
+#연습문제 8.1
+e2f = {'dog':'chien','cat':'chat','walrus':'morse'}
+#연습문제 8.2
+print(e2f.get("walrus"))
+#연습문제 8.3
+f2e = {}
+for english, french in e2f.items():
+    f2e[french] = english
+print(f2e)
+#연습문제 8.4
+print(f2e.get("chien"))
+#연습문제 8.5
+print(e2f.values())
+#연습문제 8.6
+life = {'animals' : {'cats':['Henri', 'Grumpy', 'Lucy'], 'octopi':{}, 'cmus':{}},'plants' : {},'other':{} }
+#연습문제 8.7
+print(life.keys())
+#연습문제 8.8
+print(life['animals'].keys())
+#연습문제 8.9
+print(life['animals']['cats'])
+#연습문제 8.10
+squares = {key:key*key for key in range(10)}
+print(squares)
 
-#del drinks_foods["위스키"]
-#drinks_foods["사케"] = "광어회"
-japan_drinks_foods = {"사케": "광어회", "위스키": "낙곱새"}
-drinks_foods.update(japan_drinks_foods)
 
-#drink = input(drinks_foods.keys())
-drinks_foods_keys = list(drinks_foods)
-# print(drinks_foods_keys)
-# #print(drinks_foods_keys.pop(0))
-# print(drinks_foods_keys.remove("위스키"))
-# print(drinks_foods_keys)
-#print(random.choice(drinks_foods_keys))
-
-while True:
-    menu = input(f'다음 술중에 고르세요.\n1) {drinks_foods_keys[0]}   2) {drinks_foods_keys[1]}   3) {drinks_foods_keys[2]}   4) {drinks_foods_keys[3]}   5) {drinks_foods_keys[4]}   6) 아무거나   7) 종료 : ')
-    if menu == '1':
-        print(f'{drinks_foods_keys[0]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[0]]} 입니다')
-    elif menu == '2':
-        print(f'{drinks_foods_keys[1]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[1]]} 입니다')
-    elif menu == '3':
-        print(f'{drinks_foods_keys[2]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[2]]} 입니다')
-    elif menu == '4':
-        print(f'{drinks_foods_keys[3]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[3]]} 입니다')
-    elif menu == '5':
-        print(f'{drinks_foods_keys[4]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[4]]} 입니다')
-    elif menu == '6':
-        random_drink = random.choice(drinks_foods_keys)
-        print(f'{random_drink}에 어울리는 안주는 {drinks_foods[random_drink]} 입니다')
-    elif menu == '7':
-        print(f'다음에 또 오세요')
-        break
