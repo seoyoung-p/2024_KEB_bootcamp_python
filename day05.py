2c25a090ba0d9d776b064beb272e1db1d7376643
seoyoung <psy20116@gmail.com>
2024-01-18 11:01:13 +0900

    v4.5 python day05

--- day05.py
+++ day05.py
@@ -1,3 +1,5 @@
+#prime number with function
+
 #prime number
 def isprime(n) -> bool:
     """
@@ -18,18 +20,36 @@ def isprime(n) -> bool:
             i +=1
         return True
 
-help(isprime) #함수에 대한 설명 출력
+while True:
+    menu = input("1) Fahrenheit -> Celsius   2) Celsius -> Fahrenheit   3) Prime1   4) Prime2   5) Quit program : ")
 
-numbers = input("Input first second number : ").split()
-n1 = int(numbers[0])
-n2 = int(numbers[1])
+    if menu == '1':
+        fahrenheit = float(input('Input Fahrenheit : '))
+        print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit-32.0)*5.0/9.0):.4f}C')
+    elif menu == '2':
+        celsius = float(input('Input Celsius : '))
+        print(f'Celsius : {celsius}C, Fahrenheit : {((celsius*9.0/5.0)+32.0):.4f}F')
+    elif menu == '3':
+        number = int(input("Input number : "))
+        if isprime(number):
+            print(f'{number} is prime number')
+        else:
+            print(f'{number} is NOT prime number!')
+    elif menu == '4':
+        numbers = input("Input first second number : ").split()
+        n1 = int(numbers[0])
+        n2 = int(numbers[1])
 
-if n1 > n2:
-    n1, n2 = n2, n1
+        if n1 > n2:
+            n1, n2 = n2, n1
 
-for number in range(n1, n2 + 1):
-    if isprime(number):
-        print(number, end=' ')
-print()
+        for number in range(n1, n2 + 1):
+            if isprime(number):
+                print(number, end=' ')
+        print()
 
-
+    elif menu == '5':
+        print('Terminate Program.')
+        break
+    else:
+        print('Invalid Menu!')
