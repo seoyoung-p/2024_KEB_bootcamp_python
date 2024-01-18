35d4a5eb217f05929bb4fbc8e35b622c884cdd9a
seoyoung <psy20116@gmail.com>
2024-01-18 18:16:55 +0900

    v5.1 practice  day05

--- day05.py
+++ day05.py
@@ -1,50 +1,28 @@
-# decorator
-def description(f):  # closure
-    def inner(*args):
-        print(f.__name__)
-        print(f.__doc__)
-        r = f(*args)
-        return r
+#연습문제 9.1
+def good():
+    return ['Henry', 'Ron', 'Hermione']
 
-    return inner
+print(good())
+
+#연습문제 9.2
+def get_odd():
+     num_list = [n for n in range(10) if n % 2 == 1]
+     return num_list[2]
+
+print(get_odd())
+
+#연습문제 9.3
+def trance(func):
+    def wrapper(*args, **kwargs):
+        print(func.__name__, 'start')
+        result = func(*args, **kwargs)
+        print(func.__name__, 'end')
+        return result
+    return wrapper
+@trance
+def test1():
+    print("함수를 실행하였습니다")
+
+test1()
 
 
-def squares(n):
-    """
-    제곱 함수
-    """
-    return n * n
-
-@description
-def power(b, e):
-    """
-    거듭제곱 함수
-    """
-    result = 1
-    for _ in range(e):
-        result = result * b
-    return result
-
-
-f1 = description(squares)
-print(f1(9))
-print(power(2, 10))
-# f2 = description(power)
-# print(f2(2, 10))
-
-# print(squares(7))
-# print(squares.__doc__)
-
-# def my_range(first=0, last=5, step=1):
-#     number = first
-#     while number < last:
-#         yield number
-#         number += step
-#
-# r = my_range()
-# print(r, type(r))
-#
-# for x in r:
-#     print(x)
-# for x in r:
-#     print(x)
