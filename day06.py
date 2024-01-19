a9269f5e4f59ffbcee63317a82a73529e98dfa77
seoyoung <psy20116@gmail.com>
2024-01-19 09:12:31 +0900

    v5.2 ãassignment 9.1 day06

---
+++ day06.py
@@ -0,0 +1,12 @@
+#연습문제 9.1
+def good() -> list:
+    """
+    chapter 9 things to do. 91
+    :return: list
+    """
+    harry_porter = input().split()
+    return harry_porter
+
+print(good())
+
+#연습문제 9.2
