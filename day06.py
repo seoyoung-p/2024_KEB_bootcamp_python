12cbd21ae85139388ae8617441d39a98210eafe7
seoyoung <psy20116@gmail.com>
2024-01-19 09:29:49 +0900

    v5.4 assignment 9.3(a) day06

--- day06.py
+++ day06.py
@@ -1,28 +1,21 @@
-#연습문제 9.1
-def good() -> list:
+def test(f):
     """
-    chapter 9 things to do. 91
-    :return: list
+    데코레이터 함수, 함수 시작하면 start 출력, 함수 끝나면 end 출력
+    :param f: function
+    :return: closure function
     """
-    harry_porter = input().split()
-    return harry_porter
+    #def test_in(*args, **kwargs):
+    def test_in():
+        print('start')
+        #result = f(*args, **kwargs)
+        f()
+        print('end')
+        #return result
+    return test_in
 
-print(good())
 
-#연습문제 9.2
-def get_odds(n) ->int :
-    """
-    1부터 n까지의 홀수를 발생시키는 제네레이터
-    :param n: int
-    :return: int
-    """
-    for i in range(1, n+1, 2):
-        yield i
+def greeting():
+    print("안녕하세요~")
 
-cnt = 0
-odds = get_odds(9)
-for odd in odds:
-    cnt = cnt +1
-    if cnt == 3:
-        print(f'Third number is {odd}')
-        break
+t = test(greeting)
+t()
