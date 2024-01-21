d57c8188a69bbec8ed17ddac0953036535d8d2e7
seoyoung <psy20116@gmail.com>
2024-01-21 14:25:41 +0900

    pokemon game v3.0

--- pokemongame/pokemon.py
+++ pokemongame/pokemon.py
@@ -1,3 +1,6 @@
+from random import random
+
+
 class Pokemon():
     def __init__(self, name, hp, attack_rate, defence_rate):
         self.name = name
@@ -7,11 +10,22 @@ class Pokemon():
     def attack(self, target, skill):
         attack_start = self.attack_rate + self.skills.get(skill) - target.defence_rate
         target.hp -= attack_start
-        print(f'{self.name}이 {self.skills.get(skill)}을 {target.name}에게 시전!')
+        print(f'{self.name}이(가) {skill}을(를) {target.name}에게 시전!')
         if target.hp > 0 :
             print(f'{target.name}의 hp가 {target.hp} 입니다')
         else :
             print(f'{target.name}이 사망하였습니다.')
+
+    def evolve(self):
+        can_evolve = int(random()*3)+1
+        print(can_evolve)
+        if can_evolve == 2 :
+            self.hp = self.hp * 1.2
+            self.attack_rate = self.attack_rate * 1.2
+            print(f'{self.name}이 {self.new_name}으로 진화하였습니다. 공격력과 hp가 1.2배 상승하였습니다.')
+            self.name = self.new_name
+        else :
+            print("진화에 실패하였습니다 ㅠㅠ 다음에 다시 시도해주세요.")
 
 
 class Pikachu(Pokemon):
@@ -21,27 +35,31 @@ class Pikachu(Pokemon):
         self.attack_rate = 55
         self.defence_rate = 40
         self.skills = {"정전기":10, "백만볼트":20, "피뢰침":30}
+        self.new_name = "라이츄"
 
 class Slowbro(Pokemon):
     def __init__(self):
-        self.name = "야도란" #야도란
+        self.name = "야도란"
         self.hp = 95
         self.attack_rate = 75
         self.defence_rate = 110
         self.skills = {"물대포":10, "열탕":25, "파도타기":40}
+        self.new_name = "슈퍼야도란"
 
 class Charmander(Pokemon):
     def __init__(self):
-        self.name = "파이리" #파이리
+        self.name = "파이리"
         self.hp = 39
         self.attack_rate = 52
         self.defence_rate = 43
         self.skills = {"불꽃세례":10, "화염방사":20, "불꽃튀기기":30}
+        self.new_name = "리자몽"
 
 class Squirtle(Pokemon):
     def __init__(self):
-        self.name = "파이리" #파이리
+        self.name = "파이리"
         self.hp = 44
         self.attack_rate = 48
         self.defence_rate = 65
         self.skills = {"물대포":10, "급류":20, "하이드로캐논":30}
+        self.new_name = "어니부기"
--- pokemongame/pokemon_game.py
+++ pokemongame/pokemon_game.py
@@ -17,7 +17,7 @@ while True:
         print(f'당신의 포켓몬은 {player.name} 입니다')
         break
     else :
-        print("입력 값이 잘못되었습니다.")
+        print("입력 값을 확인해 주세요.")
         break
 
 
@@ -29,21 +29,23 @@ def target_maker():
 target = target_maker()
 
 while True :
-    print("포켓몬 게임을 시작합니다.")
+
     menu = input('menu 1) 공격  2) 도망  3) 진화  4) 종료 : ')
     if menu == '1':
         player_skill = list(player.skills.keys())
         print(player_skill)
         skill = input('공격을 골라 입력해주세요 : ')
         player.attack(target, skill)
-        break
     if menu == '2':
         print("상대할 수 없는 적이 나타났습니다. 후퇴합니다!")
         target_maker()
     if menu == '3':
-        pass
+        print("진화의 돌을 주웠습니다. 진화를 시작합니다~")
+        pokemon.Pokemon.evolve(player)
+    #진화의 돌을 주울 확률 30% 진화, 성공 시 다음 포켓몬으로 세팅, 실패 시 실패메시지
     if menu == '4':
         print("게임을 종료합니다.")
+        break
 
 
 
